20c230689cecec3d5f3702c4d69cfe9f392a8918
DhinakaranS-75 <dhinakaran75493@gmail.com>
2023-07-18 11:13:41 +0530

    update code

--- database/conn.js
+++ database/conn.js
@@ -18,19 +18,4 @@ mongoose
   });
 
 
-  mongoose
-  // .connect(
-  //   "mongodb+srv://dhinakaran75493:dhinakaran75493@bikebook75.2haia60.mongodb.net/bikebook?retryWrites=true&w=majority",
-  //   {
-  //     useNewUrlParser: true,
-  //     useCreateIndex: true,
-  //     useUnifiedTopology: true,
-  //     useFindAndModify: false,
-  //   }
-  // )
-  // .then(() => {
-  //   console.log("Connected to MongoDB Atlas");
-  // })
-  // .catch((error) => {
-  //   console.error("Error connecting to MongoDB Atlas:", error);
-  // });
+  
