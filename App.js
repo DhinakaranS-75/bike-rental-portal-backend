4aebfb23ca5ab3f3b83627289b09e3e18fab5a54
DhinakaranS-75 <dhinakaran75493@gmail.com>
2023-07-24 14:41:21 +0530

    update code

--- App.js
+++ App.js
@@ -3,23 +3,15 @@ const express = require('express');
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require('cors');
+const PORT = 5000;
 
-
-dotenv.config({ path: './config.env' });
-app.use(cors())
- 
-
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
 require('./database/conn');
 app.use(require('./router/auth'));
 app.use("/uploads",express.static('uploads'));
-
-
-const PORT = 5000;
-
-
 
 app.listen(PORT, ()=>{
     console.log(`server is running on port ${PORT}`);
--- clientSideModules/signout.js
+++ clientSideModules/signout.js
@@ -1,13 +1,39 @@
+// const express = require('express');
+// const router = express.Router();
+
+// const authenticate = require("../middelware/authenticate");
+
+// // Sign Out implementation
+
+// module.exports = router.get('/signout', authenticate, (req, res)=>{
+//     console.log('user log out')
+    
+//     res.clearCookie('jwtoken', {path: '/'})
+//     res.status(200).send("User Logout")
+// })
+
 const express = require('express');
 const router = express.Router();
-
 const authenticate = require("../middelware/authenticate");
 
 // Sign Out implementation
-
-module.exports = router.get('/signout', authenticate, (req, res)=>{
-    console.log('user log out')
+router.get('/signout', authenticate, (req, res) => {
+  try {
+    console.log('User log out');
     
-    res.clearCookie('jwtoken', {path: '/'})
-    res.status(200).send("User Logout")
-})
+    // Clear the "jwtoken" cookie from the response (replace "jwtoken" with your actual token name)
+    res.clearCookie('jwtoken', { path: '/' });
+
+    // You can also clear any other cookies if necessary
+    // res.clearCookie('anotherCookie', { path: '/' });
+
+    // Send a success message to the client
+    res.status(200).send("User Logout");
+  } catch (error) {
+    console.error(error);
+    // If any error occurs, send an error response
+    res.status(500).send("Something went wrong during logout");
+  }
+});
+
+module.exports = router;
